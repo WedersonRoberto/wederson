7b1917ed2ebc3ca4da67694b2c3ff02edbe63b2e
wederson <emrferramentas@gmail.com>
2026-04-28 17:05:18 -0300

    dicionário

--- funcoes_git/funcao_2.py
+++ funcoes_git/funcao_2.py
@@ -1,5 +1,5 @@
 def exibir_info(**dados):
-    for chave, valor in dados.itens():
+    for chave, valor in dados.items():
         print(f'{chave}:{valor}')
 
 # Criamos um dicionário vazio para armazenar as entradas
@@ -9,7 +9,7 @@ info_usuario ={}
 print("Digite as informações(ou 'sair' na chave para encerrar)")
 while True:
     chave = input("Nome do campo (ex: Profissão) :")
-    if chave.lower == 'sair':
+    if chave.lower() == 'sair':
         break
     valor = input(f"Sálario {chave}:")
     info_usuario[chave]=valor

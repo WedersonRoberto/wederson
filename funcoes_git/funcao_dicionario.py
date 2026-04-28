7b3b0f8210f1d0d13df0e118e55d00e43398f254
wederson <emrferramentas@gmail.com>
2026-04-28 16:30:37 -0300

    dicionário

---
+++ funcoes_git/funcao_dicionario.py
@@ -0,0 +1,13 @@
+# Criando um dicionário
+
+produto = {"nome": "Teclado","preco":150, "em_estoque":True}
+
+# Acessando um valor
+
+print(produto["nome"])
+
+#Modificando o valor
+produto["preco"] = 135.00
+# Adicionando um novo par 
+produto["quantidade"] = 10
+print(produto)

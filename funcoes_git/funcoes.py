4cb7e1c093602bb90e19f2fc2ff7b44647e03204
wederson <emrferramentas@gmail.com>
2026-04-28 14:17:02 -0300

    valor

---
+++ funcoes_git/funcoes.py
@@ -0,0 +1,10 @@
+def formatar_real_replace(valor):
+    texto = f"R$ {valor:,.2f}" #padrão EUA 1,234.56
+    texto = texto.replace(",","X")
+    texto = texto.replace(".",",")
+    texto = texto.replace("X",".")
+    return texto
+
+#Uso
+preco = 1234.5
+print(formatar_real_replace(preco)) # 1.234,56

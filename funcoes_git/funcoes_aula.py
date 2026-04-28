48cdcbb8e10c72a7c09ca8d270473fe27bc041c0
wederson <emrferramentas@gmail.com>
2026-04-28 15:58:02 -0300

    soma

--- funcoes_git/funcoes_aula.py
+++ funcoes_git/funcoes_aula.py
@@ -16,7 +16,13 @@ criar_usuario("Dan",25,admim=True)
 def criar_perfil(nome,idade,cidade):
     print(f"{nome}, {idade} anos, {cidade}")
 criar_perfil(cidade="Curitiba" , nome="Julia" , idade=25)
-# Julia , 25 anos ,Curitiba - funciona independente da ordem !    
+# Julia , 25 anos ,Curitiba - funciona independente da ordem ! 
+
+def somar_tudo(*numeros):
+    return sum(numeros)
+print(somar_tudo(1, 2))
+print(somar_tudo(1, 2, 3, 4))
+print(somar_tudo(10, 20, 40))   
 
               
               

e5774f71e70f75cc72bd2e92092b4bcd8f60b444
wederson <emrferramentas@gmail.com>
2026-04-28 14:50:37 -0300

    função parametros

---
+++ funcoes_git/funcoes_aula.py
@@ -0,0 +1,17 @@
+#parametros com valor padrão (default)
+def saudacao(nome,mensagem="Bem-vindo!"):
+    print(f"Olá,{nome}!{mensagem}")
+
+    saudacao("Ana") # Olá ,Ana ! Bem-vindo
+    saudacao("Bob","Bom dia")#Olá Bob ! Bom dia !
+
+    # Argumentos nomeados (keyword args)
+
+    def criar_usuario(nome,idade,admim=False):
+        print(f"{nome}|{idade} anos | admim={admim}")
+
+    criar_usuario(idade=30,nome="Carol")
+    criar_usuario("Dan",25,admim=True)   
+     
+              
+              
